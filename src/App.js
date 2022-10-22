a0668c87f5d2b8b8b8790753c41e806a9f93e57b
amranhossain <amranhossainchy@gmail.com>
2022-10-22 22:00:34 +0600

    all done including local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {useState } from 'react';
+import {useEffect, useState } from 'react';
 import './App.css';
 import Todo from './Components/Todo';
 import { faListCheck} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [input, setInput] = useState('')
-  const [todos, setTodos] = useState([
-    {id : 1, task : 'shopping'},
-    {id : 2, task : 'walking'},
-    {id : 3, task : 'drinking'}
-])
+
+
+let initTodo;
+if(localStorage.getItem('todos-list') ===  null){
+  initTodo = [
+    {id: 1, task : 'bazar theke tomato ante hobe'},
+    {id: 2, task : 'current er bil dite hobe'}
+  ]
+}
+else{
+  initTodo = JSON.parse(localStorage.getItem('todos-list')) 
+}
+
+const [todos, setTodos] = useState(initTodo)
+useEffect(() => {
+  localStorage.setItem('todos-list', JSON.stringify(todos))
+}, [todos])
+
+
 
 const getTodo = (event) => {
   if(event.key === 'Enter'){
@@ -23,6 +37,7 @@ const getTodo = (event) => {
         }
         setTodos([...todos, newTodo])
         setInput('');
+        
     }
   }
 }
@@ -30,6 +45,8 @@ const deleteBtn = (item) => {
   const rest = todos.filter(todo => item!==todo);
   setTodos(rest);
 }
+
+
 
 
   return (
@@ -45,7 +62,7 @@ const deleteBtn = (item) => {
         todos.map(todo => <Todo key={todo.id} deleteBtn={deleteBtn} todo={todo}></Todo>)
       }
       {
-        todos.length === 0 && <h5>No todos left</h5>
+        todos.length === 0 && <h5>Plese add some Task</h5>
       }
     </div>
   );
